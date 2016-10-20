23f963aa0a55decb4c37e07e5ea2a1ef17e2c4e7
happysmash27 <happysmash27@vmail.me>
2016-10-19 18:35:25 -0700

    Added some content

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onload = (function() {
+    //Do nothing
+});
+
+function demoStyle(){
+    document.getElementById("pageStyle").setAttribute("href", "democrat.css")
+}
+
+function repubStyle(){
+    document.getElementById("pageStyle").setAttribute("href", "republican.css")
+}
