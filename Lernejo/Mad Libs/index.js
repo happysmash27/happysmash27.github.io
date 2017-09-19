e32130dd824861d54d239a1ce69a190e30c36011
happysmash27 <happysmash27@vmail.me>
2017-09-19 09:31:18 -0700

    Pli Bona "Mad Libs"

--- Lernejo/Mad Libs/index.js
+++ Lernejo/Mad Libs/index.js
@@ -1,6 +1,14 @@
 console.log(window.navigator.language);
-if (window.navigator.language=="en"){
+teksto=document.getElementById("teksto");
+if (window.navigator.language=="en"||true){
     console.log("en")
+    submetoj=document.getElementsByTagName("input");
+    for(var i = 0; i < submetoj.length; i++){
+	if(submetoj[i].type.toLowerCase()=="submit"){
+	    submetoj[i].value="Submit";
+	}
+    }
 } else {
     console.log("Uzas eo");
+    teksto=document.getElementById("teksto");
 }
