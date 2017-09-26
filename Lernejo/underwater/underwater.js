e6abbb94cc9942814baeafe10f89324172c490f7
happysmash27 <happysmash27@vmail.me>
2017-09-25 18:36:41 -0700

    lkfsdaj

--- Lernejo/underwater/underwater.js
+++ Lernejo/underwater/underwater.js
@@ -1,45 +1,73 @@
 console.log("Skripto funkcias");
 
 function showOctopus(){
-    if (document.getElementById("octopusFact").style["display"]==none&&document.getElementById("octopusFact").style["visibility"]=="hidden"){
+    if (document.getElementById("octopusFact").style["display"]=="none"&&document.getElementById("octopusFact").style["visibility"]=="hidden"){
 	document.getElementById("octopusFact").style["display"]="inline";
 	document.getElementById("octopusFact").style["visibility"]="visible";
-	document.getElementById("showOctopus").innerHTML="Hide Octopus Fact"
+	document.getElementById("showOctopus").innerHTML="Hide Fact"
     } else {
 	document.getElementById("octopusFact").style["display"]="none";
 	document.getElementById("octopusFact").style["visibility"]="hidden";
-	document.getElementById("showOctopus").innerHTML="Show Octopus Fact"
+	document.getElementById("showOctopus").innerHTML="Show a Fact!"
     }
 }
 
 function showAnglerfish(){
-     if (document.getElementById("anglerfishFact").style["display"]==none&&document.getElementById("anglerfishFact").style["visibility"]=="hidden"){
+    if (document.getElementById("anglerfishFact").style["display"]=="none"&&document.getElementById("anglerfishFact").style["visibility"]=="hidden"){
 	document.getElementById("anglerfishFact").style["display"]="inline";
 	document.getElementById("anglerfishFact").style["visibility"]="visible";
-	document.getElementById("showAnglerfish").innerHTML="Hide Anglerfish Fact"
+	document.getElementById("showAnglerfish").innerHTML="Hide Fact"
     } else {
 	document.getElementById("anglerfishFact").style["display"]="none";
 	document.getElementById("anglerfishFact").style["visibility"]="hidden";
-	document.getElementById("showAnglerfish").innerHTML="Show Anglerfish Fact"
+	document.getElementById("showAnglerfish").innerHTML="Show a Fact!"
     }
 }
 
 function showGoblinShark(){
-    document.getElementById("goblinSharkFact").style["display"]="inline";
-    document.getElementById("goblinSharkFact").style["visibility"]="visible";
+    if (document.getElementById("goblinSharkFact").style["display"]=="none"&&document.getElementById("goblinSharkFact").style["visibility"]=="hidden"){
+	document.getElementById("goblinSharkFact").style["display"]="inline";
+	document.getElementById("goblinSharkFact").style["visibility"]="visible";
+	document.getElementById("showGoblinShark").innerHTML="Hide Fact"
+    } else {
+	document.getElementById("goblinSharkFact").style["display"]="none";
+	document.getElementById("goblinSharkFact").style["visibility"]="hidden";
+	document.getElementById("showGoblinShark").innerHTML="Show a Fact!"
+    }
 }
 
 function showSeaLion(){
-    document.getElementById("seaLionFact").style["display"]="inline";
-    document.getElementById("seaLionFact").style["visibility"]="visible";
+    if (document.getElementById("seaLionFact").style["display"]=="none"&&document.getElementById("seaLionFact").style["visibility"]=="hidden"){
+	document.getElementById("seaLionFact").style["display"]="inline";
+	document.getElementById("seaLionFact").style["visibility"]="visible";
+	document.getElementById("showSeaLion").innerHTML="Hide Fact"
+    } else {
+	document.getElementById("seaLionFact").style["display"]="none";
+	document.getElementById("seaLionFact").style["visibility"]="hidden";
+	document.getElementById("showSeaLion").innerHTML="Show a Fact!"
+    }
 }
 
 function showManatees(){
-    document.getElementById("manateesFact").style["display"]="inline";
-    document.getElementById("manateesFact").style["visibility"]="visible";
+    if (document.getElementById("manateesFact").style["display"]=="none"&&document.getElementById("manateesFact").style["visibility"]=="hidden"){
+	document.getElementById("manateesFact").style["display"]="inline";
+	document.getElementById("manateesFact").style["visibility"]="visible";
+	document.getElementById("showManatees").innerHTML="Hide Fact"
+    } else {
+	document.getElementById("manateesFact").style["display"]="none";
+	document.getElementById("manateesFact").style["visibility"]="hidden";
+	document.getElementById("showManatees").innerHTML="Show a Fact!"
+    }
 }
 
 function showWalrus(){
-    document.getElementById("walrusFact").style["display"]="inline";
-    document.getElementById("walrusFact").style["visibility"]="visible";
+    if (document.getElementById("walrusFact").style["display"]=="none"&&document.getElementById("walrusFact").style["visibility"]=="hidden"){
+	document.getElementById("walrusFact").style["display"]="inline";
+	document.getElementById("walrusFact").style["visibility"]="visible";
+	document.getElementById("showWalrus").innerHTML="Hide Fact"
+    } else {
+	document.getElementById("walrusFact").style["display"]="none";
+	document.getElementById("walrusFact").style["visibility"]="hidden";
+	document.getElementById("showWalrus").innerHTML="Show a Fact!"
+    }
 }
