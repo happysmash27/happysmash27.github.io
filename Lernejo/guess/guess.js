c78cfc090116636828d4bbd79dbcfd581b34661f
happysmash27 <happysmash27@vmail.me>
2017-09-29 10:21:50 -0700

    Automatic reset

--- Lernejo/guess/guess.js
+++ Lernejo/guess/guess.js
@@ -1,15 +1,21 @@
 var numGuesses = 0;
 var randomNumber = Math.floor(Math.random() * 100 + 1);
+var running = true;
 
 function guess(){
-    numGuesses += 1;
-    var guess = Number(document.getElementById("guess").value);
-    if (guess == randomNumber){
-	document.getElementById("counter").innerHTML = "You won in " + numGuesses + " guesses!";
-    } else if (guess > randomNumber){
-	document.getElementById("counter").innerHTML = guess + " is too high. Guesses so far: " + numGuesses;
+    if (running==false){
+	reset();
     } else {
-	document.getElementById("counter").innerHTML = guess + " is too low. Guesses so far: " + numGuesses;
+	numGuesses += 1;
+	var guess = Number(document.getElementById("guess").value);
+	if (guess == randomNumber){
+	    document.getElementById("counter").innerHTML = "You won in " + numGuesses + " guesses!";
+	    running=false;
+	} else if (guess > randomNumber){
+	    document.getElementById("counter").innerHTML = guess + " is too high. Guesses so far: " + numGuesses;
+	} else {
+	    document.getElementById("counter").innerHTML = guess + " is too low. Guesses so far: " + numGuesses;
+	}
     }
 }
 
