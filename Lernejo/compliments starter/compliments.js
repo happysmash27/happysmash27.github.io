cf0611cb39132da7b9ff802491e52ed9dcdebff6
happysmash27 <happysmash27@vmail.me>
2017-10-26 10:20:11 -0700

    update

--- Lernejo/compliments starter/compliments.js
+++ Lernejo/compliments starter/compliments.js
@@ -4,13 +4,21 @@ var uzitajAnimaloj=[]
 var montriloAdjektiva1=0
 var montriloAdjektiva2=0
 var montriloAnimala=0
-var animaloj = ["kapro", "urso", "ruĝa vulpo"]
+var animaloj = ["kapro", "urso", "ruĝa pando", "delfeno", "polpo", "apro", "heliko", "kamelo", "muso", "rato", "kato", "hundo", "koko", "kuniklo", "testudo", "porko", "ŝafo", "bovo"]
 var adjektivoj1 = []
 var adjektivoj2 = []
-var animals = []
+var animals = ["goat", "bear", "red panda", "dolphin", "octopus", "boar", "snail", "camel", "mouse", "cat", "dog", "chicken", "rabbit", "turtle", "pig", "sheep", "neat"]
 var adjectives1 = []
 var adjectives2 = []
 
 function submit(){
-    
+    var nunaAnimalaMontrilo = Math.floor(Math.random()*animaloj.length)
+}
+
+function submetu(){
+    var nunaAnimalaMontrilo = Math.floor(Math.random()*animaloj.length)
+
+    if (animaloj[nunaAnimalaMontrilo]=="kapro"){
+	document.getElementById("imageContainer").innerHTML="<img src=\"https://upload.wikimedia.org/wikipedia/commons/9/9c/Capra_caucasica1.JPG\"/>"
+    }
 }
