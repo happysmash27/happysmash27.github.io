01280df20cb908d138b47eb87552ba60679a7532
happysmash27 <happysmash27@vmail.me>
2018-01-22 11:14:34 -0800

    While Loops Challange

--- Lernejo/While Loops Challenge/index.js
+++ Lernejo/While Loops Challenge/index.js
@@ -1,24 +1,15 @@
 function multiply(){
+    var elementID = "respondo"
     var table = document.createElement("table");
     viaNumbro = document.getElementById("viaNumbro").value;
     alKiuNumbro = document.getElementById("alKiuNumbro").value;
-    for (var i = 0; i<=alKiuNumbro; i++){
+    for (var i = 1; i<=alKiuNumbro; i++){
 	var tr = document.createElement("tr");
-	tr.appendChild(document.createTextNode())
-    }
-}
-
-function problemThree(){
-    var table = document.createElement("table")
-    var elementID = "problem-three"
-    
-    for (var i = 5; i<=30; i+=5){
-	var tr = document.createElement("tr")
-	tr.appendChild(document.createTextNode(i))
-	table.appendChild(tr)
+	tr.appendChild(document.createTextNode(i + " x " + viaNumbro + " = " + (i*viaNumbro)));
+	table.appendChild(tr);
     }
     while(document.getElementById(elementID).firstChild){
-	document.getElementById(elementID).removeChild(document.getElementById(elementID).firstChild)
+	document.getElementById(elementID).removeChild(document.getElementById(elementID).firstChild);
     }
-    document.getElementById(elementID).appendChild(table)
+    document.getElementById(elementID).appendChild(table);
 }
