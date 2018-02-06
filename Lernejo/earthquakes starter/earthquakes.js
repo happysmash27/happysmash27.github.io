d98908851187835ec1cbb3a3a6c702991e972dcd
happysmash27 <happysmash27@vmail.me>
2018-02-06 10:29:03 -0800

    earthquakes starter

--- Lernejo/earthquakes starter/earthquakes.js
+++ Lernejo/earthquakes starter/earthquakes.js
@@ -2,30 +2,31 @@ function displayQuakes(data) {
 // this is your "SUCESS FUNCTION"
 // this is where you will access and display the returned JSON information
 
-    console.log("provo 2")
-    console.log(data);
+    for (var i=0; i<3; i++){
+	document.getElementById("earthquakelist").innerHTML+=data.features[i].properties.mag + ", " + data.features[i].properties.place
+	console.log(data.features[i].properties.mag + ", " + data.features[i].properties.place)
+    }
 }
 
 
 function getAJAX() {
     $.ajax({
-	url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson", // what goes here?
-	data: {
-	    format: 'json'
-	},
-	error: function(textStatus, ajaxOptions, thrownError){
-	    console.log(textStatus);
-	    console.log(textStatus.responseText);
-	    console.log(thrownError);
-	    console.log(thrownError.filename);
-	},
-	dataType: "json",
+	url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson', // what goes here?
+	//data: {
+	    //format: 'json'
+	//},
+	//error: function(textStatus, ajaxOptions, thrownError){
+	    //console.log(textStatus);
+	    //console.log(textStatus.responseText);
+	    //console.log(thrownError);
+	    //console.log(thrownError.filename);
+	//},
+	//dataType: "json",
 	success: function(data) {
-	    console.log("Sukcesis");
 	    displayQuakes(data); // what goes here?
-	},
-	type: "GET"
+	}//,
+	//type: "GET"
     })
 }
 
-console.log("provo 1")
+//console.log("provo 1")
