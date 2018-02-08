492bf9ad8f466fede18dfd2ec0d9f8765d5ff525
happysmash27 <happysmash27@vmail.me>
2018-02-08 10:22:26 -0800

    Pli da Tertremoj

--- Lernejo/earthquakes starter/earthquakes.js
+++ Lernejo/earthquakes starter/earthquakes.js
@@ -5,11 +5,13 @@ if (lingvo === "en"){
 	document.getElementsByClassName("titolo")[i].innerHTML = "Earthquakes";
     }
     document.getElementById("earthquakeButton").innerHTML = "Earthquakes"
+    document.getElementById("PlaarthquakeButton").innerHTML = "More Earthquakes"
 } else {
     for (i in document.getElementsByClassName("titolo")){
 	document.getElementsByClassName("titolo")[i].innerHTML = "Tertremoj";
     }
     document.getElementById("earthquakeButton").innerHTML = "Tertremoj"
+    document.getElementById("PlaarthquakeButton").innerHTML = "Pli da Tertremoj"
 }
 
 function displayQuakes(data) {
@@ -23,12 +25,35 @@ function displayQuakes(data) {
     }
 }
 
+function displayQuakes2(data) {
+// this is your "SUCESS FUNCTION"
+// this is where you will access and display the returned JSON information
+
+    document.getElementById("earthquakelist").innerHTML = "";
+    for (var i=0; i<500; i++){
+	var date = new Date(data.features[i].properties.time);
+	document.getElementById("earthquakelist").innerHTML += "Magnitude " + data.features[i].properties.mag + ", " + data.features[i].properties.place + " at " + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "<br/>";
+    }
+}
+
 
 function getAJAX() {
     $.ajax({
 	url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson', // what goes here?
 	success: function(data) {
 	    displayQuakes(data); // what goes here?
+	},
+	error: function(){
+	    document.getElementById("earthquakelist").innerHTML = "Ne povis akiri datumon el earthquake.usgs.gov. Ĉu vi ebligis skriptojn de ĝi? Se ne, bonvolu ebligi tiun domajnon en via skriptblokilo. NoScript montras nur usgs.gov, kaj ebligi ĝin funkcias bone en si.";
+	}
+    })
+}
+
+function getAJAX2() {
+    $.ajax({
+	url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson', // what goes here?
+	success: function(data) {
+	    displayQuakes2(data); // what goes here?
 	},
 	error: function(){
 	    document.getElementById("earthquakelist").innerHTML = "Ne povis akiri datumon el earthquake.usgs.gov. Ĉu vi ebligis skriptojn de ĝi? Se ne, bonvolu ebligi tiun domajnon en via skriptblokilo. NoScript montras nur usgs.gov, kaj ebligi ĝin funkcias bone en si.";
