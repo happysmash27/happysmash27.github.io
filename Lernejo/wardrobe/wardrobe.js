15b8d31dc464317b3e38b9c2104991b689d4ab57
happysmash27 <happysmash27@vmail.me>
2018-02-26 11:28:47 -0800

    wardrobe project

---
+++ Lernejo/wardrobe/wardrobe.js
@@ -0,0 +1,10 @@
+
+
+function makeRequest() {
+  $.ajax({
+      url: "https://api.apixu.com/v1/current.json?key=6dffcf5b16b24d87bd7191244182602&q=" + document.getElementById("zipcode"), // what goes here?
+    success: function(data) {
+      // what goes here?
+    }
+  })
+}
