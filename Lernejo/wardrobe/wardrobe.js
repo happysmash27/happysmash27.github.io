1ffdd5de414cad5f5583123691b3c12e4ad4fa3f
happysmash27 <happysmash27@vmail.me>
2018-02-27 10:19:32 -0800

    tmp

--- Lernejo/wardrobe/wardrobe.js
+++ Lernejo/wardrobe/wardrobe.js
@@ -1,10 +1,11 @@
 
 
 function makeRequest() {
-  $.ajax({
-      url: "https://api.apixu.com/v1/current.json?key=6dffcf5b16b24d87bd7191244182602&q=" + document.getElementById("zipcode"), // what goes here?
-    success: function(data) {
-      // what goes here?
-    }
-  })
+    $.ajax({
+	url: "https://api.apixu.com/v1/current.json?key=6dffcf5b16b24d87bd7191244182602&q=" + document.getElementById("zipcode"), // what goes here?
+	success: function(data) {
+	    // what goes here?
+	    
+	}
+    })
 }
